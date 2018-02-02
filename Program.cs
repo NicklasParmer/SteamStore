93778eecdd753d3a1cf802839bcbe444051c3ed2
Nicklas <nicsi16@student.sdu.dk>
2018-02-02 11:37:27 +0100

    Store

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    public class Store
+{ 
+
+    String name;
+    float price;
+    var DLC;
+    var showcase;
+
+    public store Store;
+    {
+        games
+    }
+
+    public Game()
+    {
+
+    }
+
+    public void BuyGame(Game gameToBuy)
+    {
+        //..
+    }
+
+
+}
